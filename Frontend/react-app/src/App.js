f42e5d7a6ee0c7f97e8c9d689e05f463adb4494d
Joseph Zappala <jezap731@gmail.com>
2022-10-15 03:57:37 -0400

    Starting on posting code to backend

--- Frontend/react-app/src/App.js
+++ Frontend/react-app/src/App.js
@@ -3,8 +3,9 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
-import PersonalPage from './Pages/PersonalPage';
+import UserPage from './Pages/UserPage';
 import Friends from './Pages/Friends';
+import EditPage from './Pages/EditPage';
   
 function App() {
 return (
@@ -15,11 +16,12 @@ return (
     <Navbar />
     <Routes>
         <Route path='/'  element={<h1>WELCOME</h1>} />
-        <Route path='/mypage' element={<PersonalPage />} />
+        <Route path='/mypage' element={<UserPage />} />
         <Route path='/friends' element={<Friends />} />
-        <Route path='/blogs' element={<PersonalPage />} />
-        <Route path='/settings' element={<PersonalPage />} />
-        <Route path='/create' element={<PersonalPage />} />
+        <Route path='/blogs' element={<></>} />
+        <Route path='/settings' element={<></>} />
+        <Route path='/create' element={<></>} />
+        <Route path='/editpage' element={<EditPage />} />
     </Routes>
     </Router>
 );
--- Frontend/react-app/src/Pages/Items.js
+++ Frontend/react-app/src/Pages/Items.js
@@ -1,24 +1,48 @@
-import React from 'react';
+import {React, useState} from 'react';
 import styles from './Pages.module.css';
 import Draggable from "react-draggable";
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 
 
 function Items(props) {
+  const [open, setOpen] = useState(false);
+  const closeModal = () => setOpen(false);
+    function openPopUp() {
+      if (props.notEdit) {
+        setOpen(true)
+      }
+    }
 
     return (
       <div >
+        <Popup position="center" open={open} closeOnDocumentClick onClose={closeModal} >
+          
+          <div className={styles.leftOfPopUp}>
+            <img src={props.albumImg} alt="Album Cover Not Found"></img>
+          </div >
+            <div className={styles.rightOfPopUp}>
+              <h2>{props.albumName}</h2>
+              <button onClick={() => window.location.href=props.link}>Open spotify</button>
+          </div>
+          
+            
+        </Popup>
         
         <Draggable
         handle=".handle"
+        disabled={props.notEdit}
         defaultPosition={{x: props.xcord, y: props.ycord}}
         position={null}
         grid={[5, 5]}
         scale={1}
         >
         <div className='handle'>
-            <div className={styles.record} style={{backgroundImage: 'url(' + props.albumImg + ')', backgroundSize: "contain"}}>
+          
+          <button  onClick={openPopUp} className={styles.record} style={{backgroundImage: 'url(' + props.albumImg + ')', backgroundSize: "contain"}}>
                 <div className={styles.recordMiddle} ></div>
-            </div>
+          </button>
+            
         </div>
         </Draggable>
         
